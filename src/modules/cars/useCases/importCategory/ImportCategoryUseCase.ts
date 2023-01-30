f7297103b5fdcf8db3781e4555853781ac10a550
Daniel <ldaniel.eng@gmail.com>
2023-01-30 14:32:57 -0300

    csvParse

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,17 @@
+import fs from 'fs'
+import { parse } from 'csv-parse'
+import { Express } from 'express'
+
 export class ImportCategoryUseCase {
-  execute(file: any): void {
-    console.log(file)
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path)
+
+    const parseFile = parse()
+
+    stream.pipe(parseFile)
+
+    parseFile.on('data', async (row: any) => {
+      console.log(row)
+    })
   }
 }
