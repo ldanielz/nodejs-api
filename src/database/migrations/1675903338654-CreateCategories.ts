800390f327452650d372c09c5d9245cb3ff09443
Daniel <ldaniel.eng@gmail.com>
2023-02-09 22:02:58 -0300

    create table

--- src/database/migrations/1675903338654-CreateCategories.ts
+++ src/database/migrations/1675903338654-CreateCategories.ts
@@ -1,11 +1,40 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
 export class CreateCategories1675903338654 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isUnique: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+          },
+        ],
+      }),
+    )
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categories')
+  }
 }
